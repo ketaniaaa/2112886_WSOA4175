6374c1bbb78f62da62f9d4c1528a52bac55a17ad
Ketania Govender <2112886@students.wits.ac.za>
2022-06-08 15:11:14 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Banner from './components/Banner';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './components/Navbar';
+class App extends Component{
+    render(){    
+        return (
+<div className="App">
+  <Navbar/>
+</div>
+        );
+    }
 }
 
 export default App;
+//when you return data you can only return one piece of data and you can get around this with one html tag
+//this.props cannot be changed in property 
+//keep components modular
+//app.js to="ringo" from="john" data={1,2,3,4,5} num={3}
+//hello.js is in returning html master tag {this.props.to}! for it to say hello john
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+ class Navbar extends Component {
+  render() {
+    return (
+      <React.Fragment className="navbar">
+          <React.Fragment className="left-side">
+
+              
+          </React.Fragment>
+
+
+      </React.Fragment>
+    )
+  }
+}
+export default Navbar;
